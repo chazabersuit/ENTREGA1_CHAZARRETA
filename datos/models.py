75679ff03bbff29aba1e76246ea84db8dbe37884
Emiliano Chazarretá <chaza_bersuit@hotmail.com>
2022-07-01 23:34:09 -0300

    Se agrega base de datos

--- datos/models.py
+++ datos/models.py
@@ -1,3 +1,8 @@
+from msilib.schema import Class
 from django.db import models
 
 # Create your models here.
+class Videos(models.Model):
+    interprete=models.CharField(max_length=50)
+    album=models.CharField(max_length=50)
+    año=models.DateField()
--- datos/views.py
+++ datos/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from datos.models import Videos
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html',{})
+    
+    dato=Videos(interprete="Ibiza Pareo",album="Ibiza Pareo",año=2019)
+    dato1=Videos(interprete="Bersuit Vergarabat",album="De la cabeza",año=2002)
+
+    
+    return render(request,'index.html',{'lista_artistas':[dato, dato1]})
+
+def sobre_nosotros(request):
+    return render(request,'sobre_nosotros.html',{})
